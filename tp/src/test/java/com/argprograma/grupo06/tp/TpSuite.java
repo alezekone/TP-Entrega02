f8e6f16d61ac693fe11030ab02b98e26495d465d
Alejandro Aguilar <alezek@gmail.com>
2023-04-13 14:23:56 -0300

    Proyecto migrado de ANT a MAVEN.

---
+++ tp/src/test/java/com/argprograma/grupo06/tp/TpSuite.java
@@ -0,0 +1,38 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/UnitTests/JUnit4Suite.java to edit this template
+ */
+package com.argprograma.grupo06.tp;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.runner.RunWith;
+import org.junit.runners.Suite;
+
+/**
+ *
+ * @author mye
+ */
+@RunWith(Suite.class)
+@Suite.SuiteClasses({tp.EquipoTest.class, tp.ListaPronosticosTest.class, tp.PartidoTest.class, tp.ParticipanteTest.class})
+public class TpSuite {
+
+    @BeforeClass
+    public static void setUpClass() throws Exception {
+    }
+
+    @AfterClass
+    public static void tearDownClass() throws Exception {
+    }
+
+    @Before
+    public void setUp() throws Exception {
+    }
+
+    @After
+    public void tearDown() throws Exception {
+    }
+    
+}
